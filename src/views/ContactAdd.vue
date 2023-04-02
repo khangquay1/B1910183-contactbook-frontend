2afd5ae9f65a5ef724431deb4d5e3463d5e7f834
khangquay1 <nbana31306@gmail.com>
2023-04-02 09:00:57 +0700

    Tao trang cap nhat lien he

---
+++ src/views/ContactAdd.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="page">
+        <h4>Thêm Liên hệ</h4>
+        <ContactForm :contact="{}" @submit:contact="addContact" />
+        <p>{{ message }}</p>
+    </div>
+</template>
+<script>
+import ContactForm from "@/components/ContactForm.vue";
+import ContactService from "@/services/contact.service";
+// import router from "../router";
+export default {
+    components: {
+        ContactForm,
+    },
+    data() {
+        return {
+            contact: null,
+            message: "",
+        };
+    },
+    methods: {
+        async addContact(data) {
+            try {
+                await ContactService.create(data);
+                this.message = "Liên hệ được tạo thành công.";
+                this.$router.push({ name: "contactbook" });
+            } catch (error) {
+                console.log(error);
+            }
+        },
+    },
+};
+</script>
--- src/views/ContactBook.vue
+++ src/views/ContactBook.vue
@@ -31,7 +31,7 @@
                 <ContactCard :contact="activeContact" />
                 <router-link :to="{
                     name: 'contact.edit',
-                    params: { id: activeContact._id },git add -u
+                    params: { id: activeContact._id },
                 }">
                     <span class="mt-2 badge badge-warning">
                         <i class="fas fa-edit"></i> Hiệu chỉnh</span>
